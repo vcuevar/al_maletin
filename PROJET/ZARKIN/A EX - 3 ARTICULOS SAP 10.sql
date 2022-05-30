3e87bdea82a1addb492edacba8d00136ea9e5ae7
Vicente_CR <vcuevar@gmail.com>
2022-05-29 21:11:30 -0500

    Actual WIN 220529

--- PROJET/ZARKIN/A EX - 3 ARTICULOS SAP 10.sql
+++ PROJET/ZARKIN/A EX - 3 ARTICULOS SAP 10.sql
@@ -14,7 +14,7 @@ Declare @FechaIS nvarchar(30)
 Set @FechaCrea = CONVERT (DATE, '2022/05/19', 102)
 --Set @FechaCrea = '2022/03/30'
 -- Fecha de Inactivos Modificacion. aaaa/mm/dd
-Set @FechaInac =  CONVERT (DATE, '2022/05/19', 102)
+Set @FechaInac =  CONVERT (DATE, '2022/05/26', 102)
 
 -- Fecha 3 meses atras para enviar a Obsoletos aaa/dd/mm
 Set @FechaIS = (SELECT DATEADD(MM, -5, GETDATE()))
