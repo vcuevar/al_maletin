3032dc0e62fd545109973294e8e96ee7a599f3e0
Vicente Cueva <vcuevar@gmail.com>
2025-06-17 18:04:58 -0600

    Actual WIN 250617

--- PROJET/ZARKIN/MACROS/R015 Calculo del WIP.sql
+++ PROJET/ZARKIN/MACROS/R015 Calculo del WIP.sql
@@ -15,7 +15,7 @@ and OWOR.U_Starus = '06'
 and A3.U_TipoMat = 'PT' 
 
 /* ------------------------------------------------------------------------------------------------
-|  Para Calcular los materiales para el WIP.                                                       |
+|  Para Calcular los materiales para el WIP, EXPLOSION WIP TOTAL (SIN CA, TELA Y PIEL).            |
 --------------------------------------------------------------------------------------------------*/
 
 Select WIP.CODIGO
@@ -48,85 +48,40 @@ and A1.U_TipoMat <> 'HB'
 Group by WIP.CODIGO, WIP.MATERIAL, WIP.COSTO, WIP.UDM
 Order by WIP.MATERIAL 
 
+/* ------------------------------------------------------------------------------------------------
+|  Compara WIP VS Comprometido en SAP.                                                             |
+--------------------------------------------------------------------------------------------------*/
+
+Select OITM.ItemCode AS CODIGO
+	, OITM.ItemName as DESCRIPCION
+	, OITM.InvntryUom as UDM
+	, OITW.OnHand as IN_STOCK
+	, OITM.IsCommited AS COMPROMETIDO
+	, OITW.OnHand - OITM.IsCommited as NECESIDAD
+	, ITM1.Price as PRECIO
+	, (OITW.OnHand - OITM.IsCommited) * ITM1.Price AS IMPORTE
+	, OITM.U_Linea AS LINEA
+	, OITM.U_TipoMat AS TM
+	, ISNULL((Select Top (1) A3.ItemName AS K1 from ITT1 Inner Join OITM A1 ON ITT1.Code = A1.ItemCode 
+		Left Join OITM A3 on  ITT1.father = A3.ItemCode where ITT1.Code = OITM.ItemCode and A3.QryGroup32 = 'N'
+		Order By A3.ItemName ), 'SIN_REGISTRO')AS POR_OP
+	, ISNULL((Select Top (1) A3.ItemName AS K2 from ITT1 Inner Join OITM A1 ON ITT1.Code = A1.ItemCode 
+		Left Join OITM A3 on  ITT1.father = A3.ItemCode where ITT1.Code = OITM.ItemCode and A3.QryGroup32 = 'Y' 
+		and A3.U_TipoMat = 'SP' Order By A3.ItemName), 'SIN_REGISTRO') AS SIN_OP 
+from OITM 
+inner join OITW on OITM.ItemCode=OITW.ItemCode and OITW.WhsCode = 'APG-ST' 
+inner join ITM1 on OITM.ItemCode= ITM1.ItemCode and ITM1.PriceList= 10 
+where OITW.OnHand > 0 
+order by IMPORTE DESC, DESCRIPCION
 
 
 
 
+Select A3.ItemName AS K1 
+from ITT1 
+Inner Join OITM A1 ON ITT1.Code = A1.ItemCode 
+Left Join OITM A3 on  ITT1.father = A3.ItemCode 
+where ITT1.Code = '17774' 
+--and A3.QryGroup32 = 'N'
+Order By A3.ItemName 
 
-/*
-
-Select OWOR.DocEntry AS OP
-	, A3.ItemCode AS CODIGO
-	, A3.ItemName AS DESCRIPCION 
-	, A3.U_VS AS VS
-from OWOR 
-inner join OITM A3 on A3.ItemCode = OWOR.ItemCode 
-Where OWOR.Status ='R' and OWOR.CmpltQty < OWOR.PlannedQty 
-and OWOR.U_Starus = '06' 
-and A3.U_TipoMat = 'PT' 
-and A3.ItemCode like '3754-50%'
-
-11 375450
-
-
-
- Select WIP.CODIGO, WIP.MATERIAL AS DESCRIPCION, WIP.UDM, 'APG-ST' AS ALMACEN, SUM(WIP.ExPg) as IN_STOCK
-	, SUM(WIP.WIP) AS  COMPROMETIDO, (SUM(WIP.ExPg) -SUM(WIP.WIP)) AS NECESIDAD
-	, WIP.COSTO AS PRECIO, (SUM(WIP.ExPg) - SUM(WIP.WIP)) * WIP.COSTO AS IMPORTE, '501-200-000' AS CC
-	, WIP.LIN AS LINEA 
-From (
-
-Select WOR1.ItemCode AS CODIGO, A1.ItemName as MATERIAL, A1.InvntryUom as UDM, L10.Price AS COSTO, 0 as ExPg
-	, WOR1.BaseQty * (OWOR.PlannedQty - OWOR.CmpltQty) as WIP
-	, A1.U_Linea AS LIN 
-from WOR1 
-inner join OITM A1 on A1.ItemCode = WOR1.ItemCode 
-inner join ITM1 L10 on A1.ItemCode= L10.ItemCode and L10.PriceList= 10 
-inner join OWOR on OWOR.DocEntry = WOR1.DocEntry 
-inner join OITM A3 on A3.ItemCode = OWOR.ItemCode 
-Where OWOR.Status ='R' and OWOR.CmpltQty < OWOR.PlannedQty 
-and A1.ItmsGrpCod <> 113 
-and A1.ItmsGrpCod <> 114 
-and OWOR.U_Starus = '06' 
-and A1.InvntItem = 'Y' and A3.U_TipoMat = 'PT' 
-and OWOR.DocEntry = 64799
-
-
-
-Union all 
-
-Select OITW.ItemCode as CODIGO, OITM.ItemName as MATERIAL, OITM.InvntryUom as UDM, LPS.Price AS COSTO
-	, OITW.OnHand as ExPg, 0 as WIP, OITM.U_Linea AS LIN 
-from OITW 
-inner join OITM on OITM.ItemCode = OITW.ItemCode 
-inner join ITM1 LPS on OITM.ItemCode= LPS.ItemCode and LPS.PriceList= 10 
-Where OITW.OnHand > 0 and OITW.WhsCode = 'APG-ST' 
-and OITM.U_TipoMat <> 'PT' and OITM.InvntItem = 'Y'
-
-) WIP 
-
-Group by WIP.CODIGO, WIP.MATERIAL, WIP.COSTO, WIP.UDM, WIP.LIN Order by WIP.MATERIAL 
-   
-
-
-
-
-
-
-Select WOR1.ItemCode AS CODIGO, A1.ItemName as MATERIAL, A1.InvntryUom as UDM, L10.Price AS COSTO, 0 as ExPg
-	, WOR1.BaseQty * (OWOR.PlannedQty - OWOR.CmpltQty) as WIP
-	, A1.U_Linea AS LIN 
-from WOR1 
-inner join OITM A1 on A1.ItemCode = WOR1.ItemCode 
-inner join ITM1 L10 on A1.ItemCode= L10.ItemCode and L10.PriceList= 10 
-inner join OWOR on OWOR.DocEntry = WOR1.DocEntry 
-inner join OITM A3 on A3.ItemCode = OWOR.ItemCode 
-Where OWOR.Status ='R' and OWOR.CmpltQty < OWOR.PlannedQty 
-and A1.ItmsGrpCod <> 113 
-and A1.ItmsGrpCod <> 114 
-and OWOR.U_Starus = '06' 
-and A1.InvntItem = 'Y' and A3.U_TipoMat = 'PT' 
-and OWOR.DocEntry = 64799
-
-
-*/
